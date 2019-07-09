a61674ed58195df6ba0d5ec933c462d914535224
Jens Christian Wahl <jens.christian.wahl@nr.no>
2019-07-09 10:45:39 +0200

    add tests for sample_combinations

--- tests/testthat/test-sample_combinations.R
+++ tests/testthat/test-sample_combinations.R
@@ -1,3 +1,56 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+library(shapr)
+
+context("test-sample_combinations.R")
+
+test_that("Test function on extra.R", {
+
+  # Example -----------
+  n_train <- 10
+  n_test <- 10
+  nosamp <- 7
+  separate <- TRUE
+  cnms <- c("samp_train", "samp_test")
+
+  x <- sample_combinations(n_train, n_test, nosamp, separate)
+
+
+  # Tests -----------
+  expect_true(is.data.frame(x))
+  expect_equal(names(x), cnms)
+  expect_equal(nrow(x), nosamp)
+
+  # Expect all unique values when nosamp < n_train
+  expect_true(length(unique(x$samp_train)) == nosamp)
+  expect_true(length(unique(x$samp_test)) == nosamp)
+
+  expect_true(max(x$samp_train) <= n_train)
+  expect_true(max(x$samp_test) <= n_test)
+
+  # Example -----------
+  n_train <- 5
+  n_test <- 5
+  nosamp <- 7
+  separate <- TRUE
+
+  x <- sample_combinations(n_train, n_test, nosamp, separate)
+
+  # Tests -----------
+  expect_true(max(x$samp_train) <= n_train)
+  expect_true(max(x$samp_test) <= n_test)
+  expect_equal(nrow(x), nosamp)
+
+  # Example -----------
+  n_train <- 5
+  n_test <- 5
+  nosamp <- 7
+  separate <- FALSE
+
+  x <- sample_combinations(n_train, n_test, nosamp, separate)
+
+  # Tests -----------
+  expect_true(max(x$samp_train) <= n_train)
+  expect_true(max(x$samp_test) <= n_test)
+  expect_equal(nrow(x), nosamp)
+
+
 })
